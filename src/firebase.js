09ff86934f20c82ba0a6308f78aba8c6f7c87320
tridims <tri.dimas@student.ub.ac.id>
2023-06-12 20:32:44 +0700

    change firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,24 @@ import {
   signOut,
 } from "firebase/auth";
 
+// Testing config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBuqo-mUCcWB-xATAYeq_PApa23IjWzyVg",
+//   authDomain: "testing-auth-with-node.firebaseapp.com",
+//   projectId: "testing-auth-with-node",
+//   storageBucket: "testing-auth-with-node.appspot.com",
+//   messagingSenderId: "343964390409",
+//   appId: "1:343964390409:web:8a907bc6c11be9671354d8",
+// };
+
+// Production config
 const firebaseConfig = {
-  apiKey: "AIzaSyBuqo-mUCcWB-xATAYeq_PApa23IjWzyVg",
-  authDomain: "testing-auth-with-node.firebaseapp.com",
-  projectId: "testing-auth-with-node",
-  storageBucket: "testing-auth-with-node.appspot.com",
-  messagingSenderId: "343964390409",
-  appId: "1:343964390409:web:8a907bc6c11be9671354d8",
+  apiKey: "AIzaSyDT7mFXoGm9EFl4GZyEpQIQ1P72x-Jc08M",
+  authDomain: "capstone-skincheckai.firebaseapp.com",
+  projectId: "capstone-skincheckai",
+  storageBucket: "capstone-skincheckai.appspot.com",
+  messagingSenderId: "256420525441",
+  appId: "1:256420525441:web:46121a373d7a25b9f7a480",
 };
 
 const app = initializeApp(firebaseConfig);
